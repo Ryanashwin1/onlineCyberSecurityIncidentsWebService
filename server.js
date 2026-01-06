dda663eb5c3888ae7e0f812e69bb4aba6c5d06f0
Ryan Ashwin <23021484@myrp.edu.sg>
2026-01-06 11:59:22 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port = 3000;
+
+//database config info
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit:100,
+    queueLimit:0,
+};
+
+//initialize express app
+const app = express();
+//helps app to read JSON
+app.use(express.json());
+
+//start the server
+app.listen(port, () => {
+    console.log('Server running on port', port);
+});
+
+//Example Route: Get all incidents
+
+app.get('/allincidents', async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM incidents');
+        await connection.end();
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error for allincidents' });
+    }
+});
+
+//Example route: Create a new card
+app.post('/addincident', async (req, res) => {
+    const { card_name, card_pic } = req.body;
+
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+        await connection.execute(
+            'INSERT INTO incidents (incident_type, severity, reported_at, reference_url) VALUES (?, ?, ?, ?)',
+            [card_name, card_pic]
+        );
+        await connection.end();
+
+        res.status(201).json({
+            message: `Card ${card_name} added successfully`
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: `Server error - could not add card ${card_name}`
+        });
+    }
+});
